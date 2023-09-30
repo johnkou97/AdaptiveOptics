bc06a8f204d5751c57393bb359aa558c6009e9a9
johnkou97 <jkoutalios@gmail.com>
2023-09-30 13:46:17 +0200

    new experiments

--- experiments.py
+++ experiments.py
@@ -7,14 +7,38 @@ from gym_ao.gym_ao.gym_sharpening import Sharpening_AO_system
 env = Sharpening_AO_system()
 
 plot = LearningCurvePlot()
+
 results = []
-for run in range(5):
-    hyperparams_ac = HyperparamsActorCritic(entropy_reg=0.01, lr_actor=0.005, lr_critic=0.05, n_steps=1, batch_size=4,
+for run in range(3):
+    hyperparams_ac = HyperparamsActorCritic(entropy_reg=0.01, lr_actor=0.005, lr_critic=0.05, n_steps=2, batch_size=4, trace_length=100, 
                                             bootstrap=True, baseline=True)
     ac = ActorCritic(env, hyperparams_ac, experiment_name=f"actor_critic_run_{run}")
     rewards = ac.train(n_epochs=1000)
     results.append(smooth(rewards, 51))
 
-plot.add_curve(results, label='Actor-Critic')
-plot.save(f'experiment_actor_critic.png')
+plot.add_curve(results, label='Actor-Critic (n=2)') 
+
+
+results = []
+for run in range(3):
+    hyperparams_ac = HyperparamsActorCritic(entropy_reg=0.01, lr_actor=0.005, lr_critic=0.05, n_steps=5, batch_size=4, trace_length=100,
+                                            bootstrap=True, baseline=True)
+    ac = ActorCritic(env, hyperparams_ac, experiment_name=f"actor_critic_run_{run}")
+    rewards = ac.train(n_epochs=1000)
+    results.append(smooth(rewards, 51))
+
+plot.add_curve(results, label='Actor-Critic (n=5)')
+
+results = []
+for run in range(3):
+    hyperparams_ac = HyperparamsActorCritic(entropy_reg=0.01, lr_actor=0.005, lr_critic=0.05, n_steps=10, batch_size=4, trace_length=100,
+                                            bootstrap=True, baseline=True)
+    ac = ActorCritic(env, hyperparams_ac, experiment_name=f"actor_critic_run_{run}")
+    rewards = ac.train(n_epochs=1000)
+    results.append(smooth(rewards, 51))
+
+plot.add_curve(results, label='Actor-Critic (n=10)')
+
+
+plot.save(f'experiments/actor_critic_n_steps.png')
 
