6514445839336148c5be7a8d02a15926918be5df
johnkou97 <jkoutalios@gmail.com>
2023-10-21 19:36:52 +0200

    get learning curve

--- SAC-train.py
+++ SAC-train.py
@@ -4,7 +4,20 @@ import numpy as np
 import torch as th
 from stable_baselines3 import SAC
 from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.callbacks import BaseCallback
+from helper import LearningCurvePlot, smooth
 from gym_ao.gym_ao.gym_sharpening import Sharpening_AO_system
+
+class RewardCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super(RewardCallback, self).__init__(verbose)
+        self.rewards = []
+
+    def _on_step(self):
+        # Append the reward to the list after each step
+        self.rewards.append(self.locals["rewards"])
+        return True
+
 
 class CustomEnvWrapper(gym.Env):
     def __init__(self):
@@ -14,7 +27,11 @@ class CustomEnvWrapper(gym.Env):
         self.observation_space = gym.spaces.Box(low=0, high=1., shape=self.env.observation_space.shape, dtype=np.float32)
 
     def step(self, action):
-        observation, reward, done, _, info = self.env.step(action)
+        observation, reward, done, trunc, info = self.env.step(action)
+        if done:
+            observation = self.reset()
+        if trunc:
+            observation = self.reset()
         return observation, reward, done, info
 
     def reset(self):
@@ -26,24 +43,27 @@ class CustomEnvWrapper(gym.Env):
 # Create the Gym wrapper
 env = CustomEnvWrapper()
 
-# Create and train the SAC model
+# Create and train the SAC model with the custom callback
 model = SAC("MlpPolicy", env, verbose=1, buffer_size=10000)
-model.learn(total_timesteps=100000)  # Adjust the number of timesteps as needed
+callback = RewardCallback()
+model.learn(total_timesteps=100000, callback=callback) 
 
 # Save the trained model
 model.save("sac_sharpening")
 
-# Load the trained model if needed
-# model = SAC.load("sac_sharpening")
+# Get the rewards
+rewards = callback.rewards
+# Flatten the list of reward arrays into a single list
+rewards_flat = [item for sublist in rewards for item in sublist]
+# Save the rewards array to a file
+print(type(rewards_flat))
+np.save("rewards_sac_sharpening.npy", rewards_flat)
 
-# Test the trained model
-obs = env.reset()
-total_reward = []
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = env.step(action)
-    total_reward.append(rewards)
-    print(f'It: {i+1}, Reward: {rewards}')
+# Plot the rewards
+plot = LearningCurvePlot()
+plot.add_curve(smooth([rewards_flat], 101), "SAC")
+plot.save("sac_sharpening.png")
 
 # Close the environment
 env.close()
+
