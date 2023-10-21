9b4c6eee7e20057c59bebdab0065861fa2776838
johnkou97 <jkoutalios@gmail.com>
2023-10-21 21:29:02 +0200

    TD3 from stable_baselines3

---
+++ TD3-train.py
@@ -0,0 +1,67 @@
+import os
+import gym
+import numpy as np
+import torch as th
+from stable_baselines3 import TD3
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.callbacks import BaseCallback
+from helper import LearningCurvePlot, smooth
+from gym_ao.gym_ao.gym_sharpening import Sharpening_AO_system
+
+class RewardCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super(RewardCallback, self).__init__(verbose)
+        self.rewards = []
+
+    def _on_step(self):
+        # Append the reward to the list after each step
+        self.rewards.append(self.locals["rewards"])
+        return True
+
+class CustomEnvWrapper(gym.Env):
+    def __init__(self):
+        # Initialize your Sharpening_AO_system environment here
+        self.env = Sharpening_AO_system()
+        self.action_space = gym.spaces.Box(low=-0.3, high=0.3, shape=(400,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0, high=1., shape=self.env.observation_space.shape, dtype=np.float32)
+
+    def step(self, action):
+        observation, reward, done, trunc, info = self.env.step(action)
+        if done:
+            observation = self.reset()
+        if trunc:
+            observation = self.reset()
+        return observation, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
+
+    def render(self, mode='human'):
+        self.env.render()
+
+# Create the Gym wrapper
+env = CustomEnvWrapper()
+
+# Create and train the TD3 model with the custom callback
+model = TD3("MlpPolicy", env, verbose=1, buffer_size=10000)
+callback = RewardCallback()
+model.learn(total_timesteps=1000000, callback=callback, progress_bar=True) 
+
+# Save the trained model
+model.save("td3_sharpening")
+
+# Get the rewards
+rewards = callback.rewards
+# Flatten the list of reward arrays into a single list
+rewards_flat = [item for sublist in rewards for item in sublist]
+# Save the rewards array to a file
+print(type(rewards_flat))
+np.save("rewards_td3_sharpening.npy", rewards_flat)
+
+# Plot the rewards
+plot = LearningCurvePlot()
+plot.add_curve(smooth([rewards_flat], 101), "TD3")
+plot.save("td3_sharpening.png")
+
+# Close the environment
+env.close()
