57d78c7e7533b89042f5377cfaaebbb603939362
johnkou97 <jkoutalios@gmail.com>
2023-11-11 22:43:51 +0100

    fix bug in SAC-experiment

--- SAC-experiment.py
+++ SAC-experiment.py
@@ -23,7 +23,7 @@ def get_run_num(runs, group_name):
     return run_num
 
 # Create the Gym wrapper
-env = CustomEnvWrapper()
+env = CustomEnvWrapper(name=config["env_name"])
 
 # Create the SAC model with the custom callback
 model = SAC("MlpPolicy", env, verbose=1, buffer_size=1000)
