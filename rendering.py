1f3d96c0d051efc4ab813fd8c8c07af7453371a3
johnkou97 <jkoutalios@gmail.com>
2023-12-02 19:01:58 +0100

    gifs for sharp easy 20 zer modes

--- rendering.py
+++ rendering.py
@@ -1,35 +1,35 @@
-# evaluation of the agent vs no agent with rendering to create an animation
 from EnvironmentWrapper import CustomEnvWrapper
 import tqdm
-import numpy as np
+from stable_baselines3 import SAC
+import imageio
+import os
 
 experiment_name = "Sharpening_AO_system_easy"
 
 
 eval_episodes = 10
-eval_steps = 10
+eval_steps = 100
 
 # evaluate no agent
 
 env = CustomEnvWrapper(name=experiment_name)
 
-# average_reward = []
-# obs = env.reset()
-# for episode in tqdm.tqdm(range(eval_episodes)):
-#     rewards = []
-#     obs = env.reset()
-#     for step in range(eval_steps):
-#         action = .1
-#         obs, reward, done, info = env.step(action)
-#         rewards.append(reward)
-#         env.render(episode=episode, iteration = step, tot_rewards = average_reward, loc='no_agent')
+average_reward = []
+obs = env.reset()
+for episode in tqdm.tqdm(range(eval_episodes)):
+    rewards = []
+    obs = env.reset()
+    for step in range(eval_steps):
+        action = .1
+        obs, reward, done, info = env.step(action)
+        rewards.append(reward)
+        env.render(episode=episode, iteration = step, tot_rewards = average_reward, loc='no_agent')
     
 
-#     # keep track of rewards
-#     average_reward.append(sum(rewards)/len(rewards))
+    # keep track of rewards
+    average_reward.append(sum(rewards)/len(rewards))
 
-# evaluate agent
-from stable_baselines3 import SAC
+# # evaluate agent
 model_name = 'SAC-1.7rms-21act-100000buf-2'
 model = SAC.load(f"models/{model_name}")
 average_reward = []
@@ -46,18 +46,15 @@ for episode in tqdm.tqdm(range(eval_episodes)):
     # keep track of rewards
     average_reward.append(sum(rewards)/len(rewards))
 
-# get all the images and make a gif 
-# images are saved in the figures/animations/no_agent folder
-# each one has a name episode_iteration.png
-import imageio
-import os
+# make gifs
 images = []
 filenames = os.listdir('figures/animations/no_agent')
 filenames.sort()
 
 for filename in filenames:
-    images.append(imageio.imread('figures/animations/no_agent/' + filename))
-imageio.mimsave('figures/animations/no_agent/no_agent.gif', images, duration=0.5)
+    if filename.startswith('0') or filename.startswith('1'):
+        images.append(imageio.imread('figures/animations/no_agent/' + filename))
+imageio.mimsave('figures/animations/no_agent.gif', images, duration=1000/10)
 
 # make a gif for the agent
 images = []
@@ -65,6 +62,7 @@ filenames = os.listdir(f'figures/animations/{model_name}')
 filenames.sort()
 
 for filename in filenames:
-    images.append(imageio.imread(f'figures/animations/{model_name}/' + filename))
-imageio.mimsave(f'figures/animations/{model_name}/{model_name}.gif', images, duration=0.5)
+    if filename.startswith('3') or filename.startswith('4'):
+        images.append(imageio.imread(f'figures/animations/{model_name}/' + filename))
+imageio.mimsave(f'figures/animations/{model_name}.gif', images, duration=1000/10)
 
